380cd89bb4e5cdda110830fd6384843ed3aaa79f
Elaine Muhombe <elainees.devs@gmail.com>
2025-07-03 10:21:55 +0300

    refactor: rename function to stream_users_in_batches for clarity

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 # Simulate: SELECT * FROM user_data
-def select_all_from_user_data(batch_size):
+def stream_users_in_batches(batch_size):
     """Simulates SELECT * FROM user_data in batches."""
     current_dir = os.path.dirname(__file__)
     file_path = os.path.join(current_dir, "user_data.csv")
@@ -22,7 +22,7 @@ def select_all_from_user_data(batch_size):
 
 # Simulate: SELECT * FROM user_data WHERE age > 25
 def batch_processing(batch_size):
-    for batch in select_all_from_user_data(batch_size):
+    for batch in stream_users_in_batches(batch_size):
         filtered = (user for user in batch if user["age"] > 25)
         print(f"Filtered users over 25 from batch of {len(batch)}:")
         for user in filtered:

35ff21e4e0fe56b60a865e473604b791b8eef88b
elainees-devs <elainees-devs@gmail.com>
2025-07-08 19:05:37 +0300

    Use return statement instead of print in async functions

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,13 +5,13 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users") as cursor:
             users = await cursor.fetchall()
-            print("Users:", users)
+            return users
 
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
             older_users = await cursor.fetchall()
-            print("Older Users:", older_users)
+            return older_users
 
 async def fetch_concurrently():
     await asyncio.gather(

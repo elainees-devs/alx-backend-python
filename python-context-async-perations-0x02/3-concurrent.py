f269cae53712353b61ac7928438f02708a1db3bb
elainees-devs <elainees-devs@gmail.com>
2025-07-08 19:02:47 +0300

    Rename asyncfetch_users() and asyncfetch_older_users() to async_fetch_users() and async_fetch_older_users()

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,13 +1,13 @@
 import aiosqlite
 import asyncio
 
-async def asyncfetchusers():
+async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users") as cursor:
             users = await cursor.fetchall()
             print("Users:", users)
 
-async def asyncfetcholder_users():
+async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
             older_users = await cursor.fetchall()

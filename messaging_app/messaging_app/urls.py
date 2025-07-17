9ec137b1b025eada44bf7328aefb326314a05c43
elainees-devs <elainees.devs@gmail.com>
2025-07-17 15:30:56 +0300

    Rename 'chats.url' to 'chat.url'

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.url')),
+    path('api/', include('chat.url')),
     path('api-auth/', include('rest_framework.urls')),
 ]

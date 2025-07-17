8780df76c38ade4672d08bf66bb418eedaa90c90
elainees-devs <elainees.devs@gmail.com>
2025-07-17 15:04:39 +0300

    Add required imports and router setup to chats/urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,13 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
 
 urlpatterns = router.urls

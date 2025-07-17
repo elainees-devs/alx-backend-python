262ffad27dc610cacf53eefdfce5916a7c61e83d
elainees-devs <elainees.devs@gmail.com>
2025-07-17 15:21:46 +0300

    Use NestedDefaultRouter to nest message routes under conversations

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,11 +3,15 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 
+# Main router
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
+# Nested router
+conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup= 'conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include(conversations_router.urls))
 ]
 

320aec6a36f5e5dbef6516119e492db5ed36c596
elainees-devs <elainees.devs@gmail.com>
2025-07-22 12:49:13 +0300

    Rename 'dent_at' to 'sent_at'

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -5,7 +5,7 @@ from .models import Message
 class MessageFilters(django_filters.FilterSet):
     user = django_filters.NumberFilter(field_name='sender_id')
     created_after = django_filters.DateFilter(field_name='sent_at', lookup_expr='gte')
-    created_before = django_filters.DateFilter(field_name='dent_at', lookup_expr='lte')
+    created_before = django_filters.DateFilter(field_name='sent_at', lookup_expr='lte')
 
 
 class Meta:

7ab82968a8868f9dc2a2edca471806c1813cb8a5
elainees-devs <elainees.devs@gmail.com>
2025-07-22 11:03:06 +0300

    Enforce ownership with IsOwner permission in MessageViewSet
    - Imported and applied custom IsOwner permission to MessageViewSet
    - Ensures only participants can view messages within their conversations
    - Filtered ConversationViewSet queryset by request.user participants
    - Automatically adds request.user to conversation participants on create

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,32 +1,32 @@
 from django.shortcuts import render
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-from rest_framework.response import Response
-from rest_framework import status
+from permissions import IsOwner
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
-
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['content']
     ordering_fields = ['timestamp']
-    
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
 
     def perform_create(self, serializer):
-        # Add the request user as a participant
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
         return super().perform_create(serializer)
 
-
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    parser_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)

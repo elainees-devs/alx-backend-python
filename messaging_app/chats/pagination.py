a82028d74363b1386da4630228af4cc20175b536
elainees-devs <elainees.devs@gmail.com>
2025-07-22 12:59:09 +0300

    Customize paginated response to include total_count from paginator

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,18 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class MessagePagination(PageNumberPagination):
     page_size = 20
+
+    def get_get_paginated_response(self, data):
+        return Response({
+            'total_count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+
+        })
 

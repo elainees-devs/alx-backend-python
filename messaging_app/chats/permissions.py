b9718c28bf7d7f99ccb5b68e7027818b72a84278
elainees-devs <elainees.devs@gmail.com>
2025-07-22 11:09:25 +0300

    simplify IsOwner permission by using permissions.BasePermission
    - Removed direct import of BasePermission
    - Used permissions.BasePermission instead for clarity and consistency

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsOwner(BasePermission):
+class IsOwner(permissions.BasePermission):
     """
     Custom permission to only allow access to objects owned by the requesting user.
     """

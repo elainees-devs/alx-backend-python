bd4762af832416c74a220470b4a91ad51fc5d9cd
elainees-devs <elainees.devs@gmail.com>
2025-07-27 10:09:35 +0300

    Add logging and time-restriction middlewares for chat access

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "chats.middleware.RequestLoggingMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",
+    "chats.middleware.RestrictAccessByTimeMiddleware",
 ]
 
 

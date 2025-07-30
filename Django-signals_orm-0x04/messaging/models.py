da046ed137e739b483bf4af1371b79e1fda1829e
elainees-devs <elainees.devs@gmail.com>
2025-07-30 21:18:42 +0300

    Add MessageHistory model to track edited message content

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,6 +6,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False) # Indicates if the message has been edited
 
     def __str__(self):
         return f'Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}'
@@ -18,3 +19,11 @@ class Notification(models.Model):
 
     def __str__(self):
         return f'Notification for {self.user.username} - Message ID {self.message.id}'
+    
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'History for Message ID {self.message.id} at {self.edited_at}'

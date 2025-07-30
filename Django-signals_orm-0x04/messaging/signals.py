ba90cf5624aa03afefedc23a94876ca15056d36c
elainees-devs <elainees.devs@gmail.com>
2025-07-30 21:10:58 +0300

    Correct signal import by using post_save instead of signals.post_save

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,8 @@
-from django.db.models import signals
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Message, Notification
 
-@receiver(signals.post_save, sender=Message)   
+@receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
